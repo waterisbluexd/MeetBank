62d4c0cfb97ceefb6d4c22a37e415044da8fcc4f
waterisblue <98446139+waterisbluexd@users.noreply.github.com>
2025-11-07 01:39:26 +0530

    web working
    web

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,48 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/services.dart';
 import 'package:meetbank/screens/HomePage.dart';
 import 'package:meetbank/screens/LoginPage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
-  await Firebase.initializeApp();
-  await GoogleSignIn.instance.initialize(
-    serverClientId: "483409885225-epbnj2lsk86coqvsd7cv4sil7avm7u49.apps.googleusercontent.com",
-  );
+
+  // Set immersive mode only for Android
+  if (!kIsWeb) {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+  }
+
+  if (kIsWeb) {
+    // Web-specific Firebase initialization
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyCPp5-fEmWXmJz2cFVxyXdZeFPiGiN70PE",
+        authDomain: "meetbank-42b78.firebaseapp.com",
+        projectId: "meetbank-42b78",
+        storageBucket: "meetbank-42b78.appspot.com",
+        messagingSenderId: "483409885225",
+        appId: "1:483409885225:web:b8be92192e6f0c4391c438",
+        measurementId: "G-86CWZP05PZ",
+      ),
+    );
+  } else {
+    // Android/iOS initialization (uses google-services.json)
+    await Firebase.initializeApp();
+
+    // Initialize Google Sign-In with server client ID for Android
+    await GoogleSignIn.instance.initialize(
+      serverClientId: "483409885225-epbnj2lsk86coqvsd7cv4sil7avm7u49.apps.googleusercontent.com",
+    );
+  }
+
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
